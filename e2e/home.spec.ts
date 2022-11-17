762ad0f1cf7b482aefea3a33f46435fe2a29bdc9
Miguel Félix <87712174+miguel-felix1@users.noreply.github.com>
2022-11-17 11:59:07 +0000

    testing commit

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -4,6 +4,4 @@ test('page should have title of "Dogs security blog"', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   const title = await page.title();
   expect(title).toBe("Dogs security blog");
-  console.log("yaaaaaa");
-  console.log("e depois?");
 });
